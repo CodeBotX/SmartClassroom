a6349ebcb5831132ea3bdf9df4bd5215b8c9b01a
Minh Tiến Lê <minhtien.etbk@gmail.com>
2024-09-14 16:37:27 +0700

    cập nhật port 8080 cho frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '127.0.0.1', 
+    port: 8080          
   }
 })
