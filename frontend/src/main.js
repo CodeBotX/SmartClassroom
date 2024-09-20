cc9768421d11b0cb0bb0723be5af282d36a2941e
Minh Tiến Lê <minhtien.etbk@gmail.com>
2024-09-20 09:46:01 +0700

    Lấy folder frontend từ branch hieuminh

---
+++ frontend/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+// import css
+import './assets/css/app.css'
+
+// import router
+import router from './router'
+
+const app = createApp(App)
+  app.use(router)
+  app.mount('#app')
