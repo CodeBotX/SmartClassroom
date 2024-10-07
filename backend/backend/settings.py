fe14a8364175da478edfeec19d41d2eefd40dd54
Minh Tiến Lê <minhtien.etbk@gmail.com>
2024-10-07 08:31:51 +0700

    debug =true, cập nhật host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jws0d7!db@mj42q-*n7cqn@^+guao$@rgf3o_@@al5ailinfdr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1:8000','bkteaching.one']
 
 
 # Application definition
