ed56c6bde6d59e60b7e36aa57c5b01cf4d1d148f
Minh Tiến Lê <minhtien.etbk@gmail.com>
2024-10-07 08:22:14 +0700

    cập nhật 'api' trước enpoin

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('adminpanel/', include('adminpanel.urls')),
-    path('rooms/', include('rooms.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/adminpanel/', include('adminpanel.urls')),
+    path('api/rooms/', include('rooms.urls')),
 ]
