3b5e76b54f178db64a03f78c71cd5c8248be188f
conchimgiandoi <openworld2003@gmail.com>
2024-10-31 21:31:15 +0700

    fe pull evn 31/10

--- frontend/src/pages/LearningOutcome.vue
+++ frontend/src/pages/LearningOutcome.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="row">
-     <div class="row">
-      <div class="col-md-6">
-        <h3>Kết quả học tập</h3>
+     <div class="row ml-2">
+      <div class="">
+        <h3 class="font-weight-bold">Kết quả học tập</h3>
       </div>
-      <div class="col-md-6">
+      <!-- <div class="col-md-6">
         <div
               class="btn-group btn-group-toggle float-right"
               data-toggle="buttons"
@@ -26,7 +26,7 @@
                 {{ option }}
               </label>
             </div>
-      </div>
+      </div> -->
     </div>
           
     <div class="col-md-12" v-if="bigLineChart.activeIndex === 0">
